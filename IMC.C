e94fdee32826dabd9a6f4e609434114c2f02d46e
PC Jorge <90734777+jorgespark11@users.noreply.github.com>
2023-08-22 20:06:30 -0300

    Update Tabela de imc

--- IMC.C
+++ IMC.C
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
 int main() {
+  int mod;
+  printf("1 - Calcular o IMC \n2- Ver Tabela\n");
+  scanf("%d", &mod);
+
+  if (mod == 1){
+
     // Solicita o peso e a altura ao usuário
     float peso, altura;
     printf("Digite o peso (kg): ");
@@ -29,4 +35,37 @@ int main() {
     }
 
     return 0;
+  }
+  if (mod == 2){
+
+    printf("Relação entre Peso e Altura\n");
+    printf("===========================\n");
+    printf("Peso (kg)   |  Altura (m)\n");
+    printf("---------------------------\n");
+
+    // Loop para gerar a tabela com diferentes valores de peso e altura
+    for (float peso = 40; peso <= 120; peso += 10) {
+        for (float altura = 1.5; altura <= 2.1; altura += 0.05) {
+            float imc = peso / (altura * altura);
+
+            printf("%6.2f       |  %5.2f      |  ", peso, altura);
+
+            // Verifica a faixa de IMC e imprime o status correspondente
+            if (imc < 18.5) {
+                printf("Abaixo da média\n");
+            } else if (imc >= 18.5 && imc < 25) {
+                printf("Na média\n");
+            } else {
+                printf("Acima da média\n");
+            }
+        }
+        printf("---------------------------\n");
+    }
+
+    return 0;
+  }
+  else{
+    printf("Comando Inválido\n Encerrando...\n");
+    return 0;
+  }
 }

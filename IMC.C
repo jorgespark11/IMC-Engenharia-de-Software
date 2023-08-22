b7ea4f2f586ff38487d02171885e01df3dc6cb2c
PC Jorge <90734777+jorgespark11@users.noreply.github.com>
2023-08-22 19:51:42 -0300

    melhoria nos comentários do código

--- IMC.C
+++ IMC.C
@@ -1,24 +1,31 @@
 #include <stdio.h>
 
 int main() {
+    // Solicita o peso e a altura ao usuário
     float peso, altura;
     printf("Digite o peso (kg): ");
     scanf("%f", &peso);
     printf("Digite a altura (m): ");
     scanf("%f", &altura);
 
+    // Calcula o IMC
     float imc = peso / (altura * altura);
     printf("Seu IMC é: %.2f\n", imc);
 
+    // Verifica a faixa de IMC e fornece recomendações de saúde
     if (imc < 18.5) {
+        // Abaixo do peso ideal
         printf("Abaixo da média\n");
-        printf("Recomenda-se procurar ajuda médica\n");
+        printf("Recomenda-se procurar ajuda médica para avaliar sua nutrição e saúde geral.\n");
     } else if (imc >= 18.5 && imc < 25) {
+        // Peso ideal
         printf("Na média\n");
-        printf("Seu peso está ideal para a sua altura, porem acompanhamento médico ainda e´recomendado\n");
+        printf("Seu peso está dentro da faixa considerada saudável para a sua altura.\n");
+        printf("No entanto, é recomendado manter um estilo de vida ativo e equilibrado.\n");
     } else {
+        // Acima do peso ideal
         printf("Acima da média\n");
-        printf("Recomenda-se procurar ajuda médica\n");
+        printf("Recomenda-se procurar ajuda médica para discutir opções de gerenciamento de peso e estilo de vida.\n");
     }
 
     return 0;
